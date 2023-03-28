33cf01cdb323300de17dce44431f89bf8bc0dc35
maxivega0 <maximilianovega600@gmail.com>
2023-03-28 00:23:17 -0300

    carga de primeros enunciados de trello y ej1

---
+++ js/trello/ej2Trello.js
@@ -0,0 +1,1 @@
+//* Crear un objeto calculadora, con las propiedades y métodos necesarios para realizar las operaciones de suma, resta, multiplicación y división. Mostrar por pantalla 2 operaciones con cada método.
