7315df5ffb91c6ffd0405da93caf568885df77f3
maxivega0 <maximilianovega600@gmail.com>
2023-04-03 20:51:21 -0300

    solucion ejercicio 3 tp 4

--- js/ej3tp4.js
+++ js/ej3tp4.js
@@ -3,54 +3,33 @@
 //* 3-Escribe una clase que permita crear distintos objetos “rectángulos”, con las
 //* propiedades de alto y ancho, mas los métodos necesarios para modificar y
 //* mostrar sus propiedades, calcular el perímetro y el área
-
-class Figura{
-    #nombre;
-    #altura;
-    #base;
-    constructor(nombre,altura,base){
-        this.#nombre = nombre;
-        this.#altura = altura;
-        this.#base = base;
-    }
-    get nombre(){
-        return this.#nombre
-    }
-    set nombre(nuevoNombre){
-        if(nuevoNombre.length > 0){
-            this.#nombre = nuevoNombre
-        }
-    }
-    get altura(){
-        return this.#altura
-    }
-    set altura(nuevoAltura){
-        if(nuevoAltura.length > 0){
-            this.#altura = nuevoAltura
-        }
-    }
-    get base(){
-        return this.#base
-    }
-    set base(nuevoBase){
-        if(nuevoBase.length > 0){
-            this.#base = nuevoBase
-        }
+let alturaUsuario, baseUsuario;
+class Rectangulo{
+    constructor(altura,base){
+        this.altura = altura;
+        this.base = base;
     }
     mostrarInformacion(){
         document.write(`<ul>
-        <li>Nombre: ${this.nombre} </li>
         <li>Altura: ${this.altura} </li>
         <li>Base: ${this.base} </li>
         </ul>`)
     }
     perimetro(){
-        return this.altura * 2 + this.base * 2
+        return document.write(`El perimetro del rectangulo es de: ${this.altura * 2 + this.base * 2}<br>`)
     }
     area(){
-        return this.altura * this.base
+        return document.write(`El area del rectangulo es de: ${this.altura * this.base} `)
     }
 }
-
-const rectangulo = new Figura('Rectangulo', 5, 10)
-rectangulo.mostrarInformacion(); 
+function crearRectangulo(objeto, altura, base) {
+    objeto.altura = altura;
+    objeto.base = base;
+}
+const rectangulo = new Rectangulo();
+alturaUsuario = Number(prompt("Ingrese la altura."));
+baseUsuario = Number(prompt("Ingrese la base."));
+crearRectangulo(rectangulo, alturaUsuario,baseUsuario);
+rectangulo.mostrarInformacion();
+rectangulo.perimetro();
+rectangulo.area();
